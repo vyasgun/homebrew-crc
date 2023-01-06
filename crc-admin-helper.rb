07daab64c0aed6d9b3b8c8b3811ecc5517ef584b
Christophe Fergeau <cfergeau@redhat.com>
2023-01-06 11:30:01 +0100

    crc-admin-helper: Install to $bindir
    libexec does not seem to be supported very well, there is no
    /opt/homebrew/libexec dir similar to /opt/homebrew/bin
    crc-admin-helper can go to /opt/homebrew/bin instead.

--- crc-admin-helper.rb
+++ crc-admin-helper.rb
@@ -11,16 +11,16 @@ class CrcAdminHelper < Formula
 
   def install
     system "make", "all"
-    libexec.install "crc-admin-helper"
+    bin.install "crc-admin-helper"
   end
 
   test do
     # test version
-    version_output = shell_output("#{libexec}/crc-admin-helper --version 2>&1").strip
+    version_output = shell_output("#{bin}/crc-admin-helper --version 2>&1").strip
     assert_match(/admin-helper version #{version}/, version_output)
 
     # check for the presence of a non existing hostname
-    assert_equal("false", shell_output("#{libexec}/crc-admin-helper contains brew-test.example.com 1.2.3.4").strip)
+    assert_equal("false", shell_output("#{bin}/crc-admin-helper contains brew-test.example.com 1.2.3.4").strip)
   end
 end
 
