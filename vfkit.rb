02665a7be3ffa987b84db435aa9244c7d045bc8d
Gunjan Vyas <vyasgun20@gmail.com>
2025-02-12 14:57:39 +0530

    Vfkit formula changes

--- vfkit.rb
+++ vfkit.rb
@@ -21,11 +21,10 @@ class Vfkit < Formula
   test do
     # test version
     version_output = shell_output("#{bin}/vfkit --version 2>&1").strip
-    assert_match(/vfkit version: #{version}/, version_output)
+    assert_match(/vfkit version: v#{version}/, version_output)
 
     # start a VM with non-existing kernel
     lines =  shell_output("#{bin}/vfkit --kernel foo --initrd bar --kernel-cmdline baz 2>&1", 1).strip.split(/\r?\n|\r/)
     assert_match(/open foo: no such file or directory/, lines.last)
   end
 end
-
